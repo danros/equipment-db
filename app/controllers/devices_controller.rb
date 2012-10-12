8f311f89f5a104027be6b654bc9325adb334310b
Phil Cowans <phil@philcowans.com>
2012-10-13 00:12:15 +0100

    Removing logging

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,7 +2,6 @@ class DevicesController < ApplicationController
   before_filter :require_authentication
 
   def create
-    logger.info params.inspect
     device = Device.create(params[:device])
     if device.valid?
       redirect_to device_path(device), :status => 303
