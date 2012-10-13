a7b572fd209525dd1f8d802bf97d97685d87dbdc
Phil Cowans <phil@philcowans.com>
2012-10-13 21:42:57 +0100

    More UI tidy-up

--- app/models/page_models/home/show/device_row.rb
+++ app/models/page_models/home/show/device_row.rb
@@ -5,6 +5,22 @@ module PageModels
         def initialize(device)
           @device = device
         end
+
+	def name
+	  @device.name
+	end 
+
+	def path
+          "/devices/#{@device.id}"
+	end
+	
+	def status
+ 	  '(status)'
+	end 
+
+	def tag
+	  '(tag)'
+	end 
       end
     end
   end
