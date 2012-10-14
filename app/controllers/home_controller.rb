98bd4e4e3cdc73e44e9890d65b3ce99149f4d314
Phil Cowans <phil@philcowans.com>
2012-10-14 18:26:55 +0100

    Sort list of devices by name so it's predictable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_filter :require_authentication
 
   def show
-    devices = Device.find(:all)
+    devices = Device.find(:all, :order => 'name')
     @page = PageModels::Home::Show.new(devices)
   end
 end
