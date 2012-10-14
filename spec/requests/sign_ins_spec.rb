3821dd1e86fd4794f5ff20e0c8566062bb47c6c2
Rich Wareham <rjw57@cam.ac.uk>
2012-10-14 12:52:02 +0100

    add sign out support
    Using the new destroy action in the sessions controller, add in support
    for a sign out link as well as a sign in link. Update the tests to check
    that signing in and out do the expected things.

--- spec/requests/sign_ins_spec.rb
+++ spec/requests/sign_ins_spec.rb
@@ -1,17 +1,35 @@
 require 'spec_helper'
 
-describe "Signing in" do
-  describe "doesn't happen by default" do
-    it "and has a sign-in link" do
+describe "before signing in" do
+  describe "the home page" do
+    it "has a sign-in link" do
       visit root_path
       page.should have_link('Sign in', href: new_session_path)
     end
 
-    it "and has a sign-in button" do
+    it "has a sign-in button" do
       visit root_path
       within('.hero-unit') do
         page.should have_link('Sign in', href: new_session_path)
       end
     end
+  end
+end
+
+describe "after signing in" do
+  before do
+    visit new_session_path
+    fill_in 'Username:', with: 'FIXME: ignored'
+    fill_in 'Password:', with: 'mak3spac3'
+    click_button 'Sign in'
+  end
+
+  it "removes sign in link" do
+    visit root_path
+    page.should_not have_link('Sign in', href: new_session_path)
+  end
+
+  it "adds a sign out link" do
+    page.should have_link('Sign out', href: signout_path)
   end
 end
