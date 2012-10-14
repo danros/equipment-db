6837e05209644e145e469259e554cfe69dc6418f
Rich Wareham <rjw57@cam.ac.uk>
2012-10-14 12:20:20 +0100

    prettify default forbidden page
    Add a test to check that it has a Bootstrap 'hero unit' and that the
    hero unit contains a link to the sign in page.

--- spec/requests/sign_ins_spec.rb
+++ spec/requests/sign_ins_spec.rb
@@ -6,5 +6,12 @@ describe "Signing in" do
       visit root_path
       page.should have_link('Sign in', href: new_session_path)
     end
+
+    it "and has a sign-in button" do
+      visit root_path
+      within('.hero-unit') do
+        page.should have_link('Sign in', href: new_session_path)
+      end
+    end
   end
 end
