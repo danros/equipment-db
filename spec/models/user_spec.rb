79c511c7e066d16cb3bd36e6ad17381ef1834056
Rich Wareham <rjw57@cam.ac.uk>
2012-10-16 19:10:08 +0100

    User: update comment annotation of schema
    This is a comment-only commit. It should therefore have no functional
    changes :).

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,26 @@
 #
 # Table name: users
 #
-#  id              :integer          not null, primary key
-#  name            :text             not null
-#  email           :text             not null
-#  active          :boolean          default(TRUE), not null
-#  password_digest :text
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
+#  id                     :integer          not null, primary key
+#  name                   :text             not null
+#  email                  :text             not null
+#  active                 :boolean          default(TRUE), not null
+#  password_digest        :text             default(""), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  confirmation_token     :string(255)
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string(255)
 #
 
 require 'spec_helper'
