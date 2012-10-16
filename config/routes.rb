132a8045f7f6d13b6edcf873c55dd2d333398c5f
Rich Wareham <rjw57@cam.ac.uk>
2012-10-16 18:50:40 +0100

    remove controllers which are now unused
    Devise provides controllers for both session and user management.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,4 @@ EquipmentDb::Application.routes.draw do
   resources :devices, :only => [:create, :edit, :new, :show, :update] do
     resources :maintainers, :only => [:create, :destroy, :new]
   end
-  resources :sessions, :only => [:create, :new]
-  #resources :users, :only => [:create, :new, :show]
-
-  match '/signout', to: 'sessions#destroy'
 end
