2d94e854769d3bf6748308ea904db42d67e66211
Phil Cowans <phil@philcowans.com>
2012-10-17 21:58:57 +0100

    I think the ActionMailer tutorial I followed earlier was out of date

--- app/mailers/maintenance_alert_mailer.rb
+++ app/mailers/maintenance_alert_mailer.rb
@@ -1,9 +1,7 @@
 class MaintenanceAlertMailer < ActionMailer::Base
   def status_change(user, device)
-    recipients "#{user.name} <#{user.email}>"
-    from "Makespace Equipment Database"
-    subject "[Makespace Maintenance Alert] #{device.name} marked as #{device.status.name}"
-    sent_on Time.now
-    body {:user => user, :device => device}
+    @user = user
+    @device = device
+    mail(:to => user.email, :subject => "[Makespace Maintenance Alert] #{device.name} marked as #{device.status.name}")
   end
 end
