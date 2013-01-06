82e1acb57b5aea035ccfe68661d84dca5c690c07
Phil Cowans <phil@philcowans.com>
2013-01-06 10:38:27 +0000

    Don't require owners to have unique email addresses.

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -22,6 +22,5 @@ class Owner < ActiveRecord::Base
   # A very light-touch email verification. The only thing one can really state
   # with certainty is there should be an '@' sign which isn't at either end.
   validates :email, presence:   true,
-                    format:     { with: /.+@.+/ },
-                    uniqueness: { case_sensitive: false }
+                    format:     { with: /.+@.+/ }
 end
