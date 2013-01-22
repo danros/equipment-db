89c1159dfbc5cc09ece6e01a3b5c90f60faf666f
Phil Cowans <phil@philcowans.com>
2013-01-22 19:51:52 +0000

    Add exception to auth for Makespace local IP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def require_authorized_user_with_local_exception
+    return if request.remote_ip == '86.162.158.158'
+    require_authorized_user
+  end
+
   def require_authorized_user
     render :template => 'sessions/unauthorized', :status => 401 unless current_user.authorized?
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :require_authorized_user
+  before_filter :require_authorized_user_with_local_exception
 
   def show
     categories = Category.find(:all, :order => 'name')
