b94e038e74f64e74a8079be2112686f136775f38
Phil Cowans <phil@philcowans.com>
2013-01-22 20:19:00 +0000

    Maybe I don't know how devise works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def authenticate_user_with_local_exception
+    return if request.remote_ip == '86.162.158.158'
+    authenticate_user!
+  end
+
   def require_authorized_user_with_local_exception
-    logger.error "Remote IP is #{request.remote_ip}"
     return if request.remote_ip == '86.162.158.158'
     require_authorized_user
   end
