1957129bb85cd1956dc10f09e13a9ff7f1e6fc4d
Phil Cowans <phil@philcowans.com>
2013-03-03 18:53:30 +0000

    Removed additional filter on index page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!
-
   def show
     categories = Category.find(:all, :order => 'name')
     uncategorised_devices = Device.find(:all, :conditions => 'category_id is null or category_id = -1', :order => 'name')
