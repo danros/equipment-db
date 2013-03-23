befd6fcc4e5f3277a1884c41a37c779db5588f0f
Phil Cowans <phil@philcowans.com>
2013-03-23 22:02:21 +0000

    Typo

--- app/controllers/wiki_pages_controller.rb
+++ app/controllers/wiki_pages_controller.rb
@@ -1,6 +1,6 @@
 class WikiPagesController < ApplicationController
   def show
-    device = Device.find(:asset_code => params[:id])
+    device = Device.find(:first, :asset_code => params[:id])
     if(device.reference_url)
       redirect_to device.reference.url
     else
