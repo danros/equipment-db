be6fb97f681ed00d67c276de6427f75d1bb54793
Phil Cowans <phil@philcowans.com>
2013-03-23 22:08:02 +0000

    .

--- app/controllers/wiki_pages_controller.rb
+++ app/controllers/wiki_pages_controller.rb
@@ -2,7 +2,7 @@ class WikiPagesController < ApplicationController
   def show
     device = Device.find(:first, :conditions => {'asset_code' => params[:id]})
     if(device.reference_url)
-      redirect_to device.reference.url
+      redirect_to device.reference_url
     else
       render :text => 'Not found', :status => 404
     end
